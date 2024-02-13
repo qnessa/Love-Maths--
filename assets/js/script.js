c0639538bcc9c3a57ad6af9d594ef144f909ca9a
Vanessa Ilombe <vnsssitailombe@googlemail.com>
2024-02-13 12:48:00 +0000

    Added substraction and multiplication game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,10 +31,14 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+} else if (gameType === "subtract" ) {
+    displaySubtractQuestion(num1, num2);
 } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
-}
+ }
 
 }
 /**
@@ -69,6 +73,10 @@ let operand1 = parseInt(document.getElementById('operand1').innerText);
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -98,9 +106,16 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
-function displayMultiplyQuestion() {   
+function displayMultiplyQuestion(operand1, operand2) { 
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";  
 }
